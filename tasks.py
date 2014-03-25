c34785378e04730ac2509cfa1046f44e885fb55e
Robin Hahling <robin.hahling@gw-computing.net>
2014-03-25 21:46:54 +0100

    create tasks to setup the application

--- tasks.py
+++ tasks.py
@@ -1,3 +1,5 @@
+from shutil import copy
+import sys
 from invoke import (
     task,
     run
@@ -5,11 +7,54 @@ from invoke import (
 
 # define projects directories
 tools_dir = 'tools'
+devmine_dir = 'devmine'
+config_dir = devmine_dir + '/config'
+test_dir = devmine_dir + '/test'
+func_test_dir = test_dir + '/functional'
+unit_test_dir = test_dir + '/unit'
+
+
+@task
+def set_settings(environment='production', nosetests=''):
+    if environment not in ['production', 'development', 'test']:
+        print('Error: ' + environment + ' is not a valid parameter',
+              file=sys.stderr)
+        exit(1)
+
+    src = config_dir + '/settings-' + environment + '.py.sample'
+    dst = config_dir + '/settings.py'
+    print('Copying ' + src + ' to ' + dst)
+    copy(src, dst)
+    print('Done')
+
+
+@task('set_settings')
+def test_func(environment='test', nosetests='nosetests'):
+    run_cmd(nosetests + ' -w ' + func_test_dir)
+
+
+@task('set_settings')
+def test_unit(environment='test', nosetests='nosetests'):
+    run_cmd(nosetests + ' -w ' + unit_test_dir)
+
+
+@task('set_settings', 'test_func', 'test_unit')
+def test(environment='test', nosetests='nosetests'):
+    pass
+
+
+@task('set_settings')
+def setup():
+    src = 'alembic.ini.sample'
+    dst = 'alembic.ini'
+    print('Copying ' + src + ' to ' + dst)
+    copy(src, dst)
+    print('Done')
 
 
 @task
 def pep8():
-    cmd = 'pep8 tasks.py ' + tools_dir
+    cmd = 'pep8 run.py tasks.py ' + devmine_dir + ' ' + tools_dir
     run_cmd(cmd)
 
 
