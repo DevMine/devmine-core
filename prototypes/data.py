fe73c8579ce78f461da5b2fea1fc8e9e7d440c25
Daniel 'Akathorn' Espino <akathorn@gmail.com>
2014-04-05 14:18:01 +0200

    implements class JsonData that reads from .json files in the prototype

--- prototypes/data.py
+++ prototypes/data.py
@@ -1,6 +1,7 @@
 """This file provides abstraction over the source of data used in the server"""
 
 import random
+import json
 
 
 class DataSource:
@@ -16,13 +17,70 @@ class RandomData(DataSource):
         print(r.random())
 
         self.scores = {}
-        self.features = ["Python", "Cookies", "Chocolate",
-                         "Superpowers", "Doge"]
-        self.developers = ["Robin", "Kevin", "Laurent", "Xuan",
-                           "Frederik", "Daniel", "Clément"]
+        self.features = ['Python', 'Cookies', 'Chocolate',
+                         'Superpowers', 'Doge']
+        self.developers = ['Robin', 'Kevin', 'Laurent', 'Xuan',
+                           'Frederik', 'Daniel', 'Clément']
 
         for developer in self.developers:
             scores = {}
             for feature in self.features:
                 scores[feature] = r.random()
             self.scores[developer] = scores
+
+
+class RandomData(DataSource):
+    def __init__(self, seed=None):
+        r = random.Random(seed)
+        print(r.random())
+
+        self.scores = {}
+        self.features = ['Python', 'Cookies', 'Chocolate',
+                         'Superpowers', 'Doge']
+        self.developers = ['Robin', 'Kevin', 'Laurent', 'Xuan',
+                           'Frederik', 'Daniel', 'Clément']
+
+        for developer in self.developers:
+            scores = {}
+            for feature in self.features:
+                scores[feature] = r.random()
+            self.scores[developer] = scores
+
+
+class JsonData(DataSource):
+    def __init__(self, dev_file, repo_file):
+
+        devs = json.load(open(dev_file))
+        repos = json.load(open(repo_file))
+
+        self.developers = []
+        self.scores = {}
+
+        for dev in devs:
+            dev_repos = [repo for repo in repos
+                         if repo['owner']['login'] == dev['login']]
+
+            self.developers.append(dev['login'])
+            self.scores[dev['login']] = self.compute_scores(dev, dev_repos)
+
+        # Compute the list of features
+        self.features = []
+        for v in self.scores.values():
+            for k in v.keys():
+                self.features.append(k)
+
+
+    def compute_scores(self, dev, repos):
+        score = {}
+
+        score['followers'] = dev['followers']
+        score['public_repos'] = dev['public_repos']
+
+        for repo in repos:
+            if repo['language'] != None:
+                if repo['language'] in score.keys():
+                    score[repo['language']] += int(repo['size'])
+                else:
+                    score[repo['language']] = int(repo['size'])
+
+        return score
