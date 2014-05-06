e9efb5e2ba19fcda77e35d0efdaa03b13d025df0
Robin Hahling <robin.hahling@gw-computing.net>
2014-05-06 15:19:57 +0200

    create model of a feature

---
+++ devmine/app/models/feature.py
@@ -0,0 +1,19 @@
+from sqlalchemy import (
+    Column,
+    Integer,
+    String
+)
+
+from devmine.app.models import Base
+
+
+class Feature(Base):
+    """Model of a feature."""
+
+    __tablename__ = 'features'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False, unique=True)
+
+    def __init__(self):
+        pass
