d3ee1a6d8759c9dfc806fdaf8c49c69d337fcc6f
Robin Hahling <robin.hahling@gw-computing.net>
2014-05-06 15:23:12 +0200

    add model of a user

---
+++ devmine/app/models/user.py
@@ -0,0 +1,19 @@
+from sqlalchemy import (
+    Column,
+    Integer,
+    String
+)
+
+from devmine.app.models import Base
+
+
+class Feature(Base):
+    """Model of a user."""
+
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    login = Column(String, nullable=False, unique=True)
+
+    def __init__(self):
+        pass
