91b252925fd15b405fcd97271e23274e84b14c19
Kevin Gillieron <kevin.gillieron@gw-computing.net>
2014-05-10 13:47:19 +0200

    add method for serializing an object to JSON

--- devmine/app/helpers/application_helper.py
+++ devmine/app/helpers/application_helper.py
@@ -1,6 +1,19 @@
 import json
 from sqlalchemy.ext.declarative import DeclarativeMeta
 
+def obj_to_json(obj):
+    """
+    Serialize an object to JSON.
+    """
+    fields = {}
+    attrs = [x for x in obj.__dict__.keys() if not x.startswith('_')]
+    for field in attrs:
+        data = obj.__getattribute__(field)
+        if hasattr(data, 'isoformat'):
+            data = data.isoformat()
+        json.dumps(data)
+        fields[field] = data
+    return fields
 
 class AlchemyEncoder(json.JSONEncoder):
     """
@@ -9,14 +22,5 @@ class AlchemyEncoder(json.JSONEncoder):
     """
     def default(self, obj):
         if isinstance(obj.__class__, DeclarativeMeta):
-            fields = {}
-            attrs = [x for x in obj.__dict__.keys() if not x.startswith('_')]
-            for field in attrs:
-                data = obj.__getattribute__(field)
-                if hasattr(data, 'isoformat'):
-                    data = data.isoformat()
-                json.dumps(data)
-                fields[field] = data
-            return fields
-
+            return obj_to_json(obj)
         return json.JSONEncoder.default(self, obj)
