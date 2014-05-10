98b0d83dac5f1950bf3a06515c3767837408bc68
Kevin Gillieron <kevin.gillieron@gw-computing.net>
2014-05-10 13:51:42 +0200

    define API for getting the features grouped by category

---
+++ devmine/app/controllers/features_controller.py
@@ -0,0 +1,32 @@
+import json
+from io import StringIO
+from bottle import abort
+import logging
+from devmine.app.models.feature import Feature
+from devmine.app.controllers.application_controller import (
+    ApplicationController
+)
+from devmine.app.helpers import application_helper as ah
+
+
+class FeaturesController(ApplicationController):
+    """Class for handling requests on the feature resource."""
+
+    def by_category(self, db, category):
+        """Return all features sorted by category as a JSON string
+        like the following:
+            {'category1': ['feature1': {...}, ...], 'category2': [...]}
+        """
+        features = db.query(Feature).order_by(
+            Feature.category, Feature.name).all()
+
+        if len(features) == 0:
+            return ""
+
+        retval = {}
+        for f in features:
+            if f.category not in retval:
+                retval[f.category] = []
+            retval[f.category].append(ah.obj_to_json(f))
+
+        return json.dumps(retval)
--- devmine/config/routes.py
+++ devmine/config/routes.py
@@ -1,7 +1,10 @@
 from devmine.app.controllers.index_controller import IndexController
 from devmine.app.controllers.search_controller import SearchController
+from devmine.app.controllers.features_controller import FeaturesController
 
 
 def setup_routing(app):
     app.route('/', 'GET', IndexController().index)
     app.route('/search/<q>', 'GET', SearchController().query)
+    app.route('/features/by_category/<category>', 'GET',
+              FeaturesController().by_category)
